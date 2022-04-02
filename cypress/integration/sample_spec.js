d8817dbce64226af45fd4f23b69f93235a500407
presi11 <juan.ospina19@udea.edu.co>
2022-04-02 08:43:07 -0500

    first commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,10 @@
+describe('My First Cypress Test', () => {
+    it('Assert Example', () => {
+      expect(true).to.equal(true)
+    })
+
+    it('Visits cypress test webpage ', () => {
+      cy.visit('https://example.cypress.io/')
+      cy.contains('type').click()
+    })
+})
